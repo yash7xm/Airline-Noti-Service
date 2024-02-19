d30d7024158cb05d50f1f55a77d5b276c22827d2
yash7xm <yash.7xm@gmail.com>
2024-02-19 23:33:34 +0530

    added node mailer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-const express = require('express');
+const express = require("express");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
+
+const mailsender = require("./config/email-config");
 
 const app = express();
 
-app.use('/api', apiRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, async () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  try {
+    const response = await mailsender.sendMail({
+      from: ServerConfig.GMAIL_EMAIL,
+      to: "yash.7xm@gmail.com",
+      subject: "Is the service working ? now as well",
+      text: "Yes it is working",
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
