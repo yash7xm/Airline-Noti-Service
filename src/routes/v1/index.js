2ea774d8f7985e28eae8e4744b768170f7f8977a
yash7xm <yash.7xm@gmail.com>
2024-02-20 00:06:48 +0530

    added create ticket api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 
-const { InfoController } = require('../../controllers');
+const { InfoController } = require("../../controllers");
+const { EmailController } = require("../../controllers");
 
 const router = express.Router();
 
-router.get('/info', InfoController.info);
+router.get("/info", InfoController.info);
+
+router.get("/tickets", EmailController.create);
 
 module.exports = router;
