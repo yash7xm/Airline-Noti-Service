fa99c39c21a6a205c7f5a96bac826d3faee2709a
yash7xm <yash.7xm@gmail.com>
2024-02-20 01:07:12 +0530

    completed consumption and sending mail

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ServerConfig: require('./server-config'),
-    Logger: require('./logger-config'),
-    MAILER: require('./email-config')
-}
+  ServerConfig: require("./server-config"),
+  Logger: require("./logger-config"),
+  MAILER: require("./email-config"),
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const amqplib = require("amqplib");
+const { EmailService } = require("./services");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
@@ -10,6 +12,28 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
+async function connectQueue() {
+  try {
+    const connection = await amqplib.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    await channel.assertQueue("noti-queue");
+    channel.consume("noti-queue", async (data) => {
+      const object = JSON.parse(`${Buffer.from(data.content)}`);
+      await EmailService.sendEmail(
+        "yash.7xm@gmail.com",
+        object.recepientEmail,
+        object.subject,
+        object.text
+      );
+      channel.ack(data);
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  await connectQueue();
+  console.log("queue is up");
 });
