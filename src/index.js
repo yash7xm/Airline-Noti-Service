2039beec82372b4807bd032f6d9e6adde3f7f712
yash7xm <yash.7xm@gmail.com>
2024-02-19 23:48:41 +0530

    basic repositories added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
-const mailsender = require("./config/email-config");
-
 const app = express();
 
 app.use(express.json());
@@ -14,15 +12,4 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-  try {
-    const response = await mailsender.sendMail({
-      from: ServerConfig.GMAIL_EMAIL,
-      to: "yash.7xm@gmail.com",
-      subject: "Is the service working ? now as well",
-      text: "Yes it is working",
-    });
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
 });
